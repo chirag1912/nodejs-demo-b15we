79d8cd8310aa50704afa2af293a8fa8be7cc3baa
Venkatasubramanian <pvenkat.rv@gmail.com>
2020-11-22 12:26:54 +0530

    auth

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongodb = require("mongodb");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const bcrypt = require("bcrypt");
 
 const app = express();
 dotenv.config();
@@ -75,6 +76,55 @@ app.delete("/delete-user/:id", async (req, res) => {
       .collection("users")
       .deleteOne({ _id: objectId(req.params.id) });
     res.status(200).json({ message: "User deleted" });
+    client.close();
+  } catch (error) {
+    console.log(error);
+    res.send(500);
+  }
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    let client = await mongodb.connect(dbUrl);
+    let db = client.db("B15_WE");
+    let data = await db.collection("users").findOne({ email: req.body.email });
+    if (data) {
+      res.status(400).json({
+        message: "User already exists",
+      });
+    } else {
+      let salt = await bcrypt.genSalt(10);
+      let hash = await bcrypt.hash(req.body.password, salt);
+      req.body.password = hash;
+      let result = await db.collection("users").insertOne(req.body);
+      res.status(200).json({
+        message: "User registered",
+      });
+    }
+    client.close();
+  } catch (error) {
+    console.log(error);
+    res.send(500);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    let client = await mongodb.connect(dbUrl);
+    let db = client.db("B15_WE");
+    let data = await db.collection("users").findOne({ email: req.body.email });
+    if (data) {
+      let isValid = await bcrypt.compare(req.body.password, data.password);
+      if (isValid) {
+        res.status(200).json({ message: "Login success" });
+      } else {
+        res.status(401).json({ message: "Login unsuccessful" });
+      }
+    } else {
+      res.status(400).json({
+        message: "User is not registered",
+      });
+    }
     client.close();
   } catch (error) {
     console.log(error);
